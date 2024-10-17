cd63b7f79991cae5de60ddc39dbd1a061e749350
Ragib Shahrier Bin Kabir <ragibshahrier2004123@gmail.com>
2024-10-17 14:26:43 +0600

    add blackhole cooldown

--- game/Assets/scripts/player/BlackholeSpawner.cs
+++ game/Assets/scripts/player/BlackholeSpawner.cs
@@ -8,8 +8,10 @@ public class BlackholeSpawner : MonoBehaviour
     [SerializeField] private GameObject blackholePrefab;
     [SerializeField] private GameObject shield;
     [SerializeField] private float blackholeLifetime = 5.0f;
+    [SerializeField] private float blackHoleCoolDowntime = 5.0f;
     [SerializeField] private Animator animator;
     private float timeCounter = 0.0f;
+
     private Vector3 initialScale;
     void Awake()
     {
@@ -42,14 +44,11 @@ public class BlackholeSpawner : MonoBehaviour
                 Debug.Log("timeout");
                 animator.SetBool("timeout", true);
             }
-            else{
-                timeCounter += Time.deltaTime;
-            }
             
-            
-            
+               
         }
-        if(Input.GetKeyDown(KeyCode.B) && !blackholePrefab.activeSelf){
+        timeCounter += Time.deltaTime;
+        if(Input.GetKeyDown(KeyCode.B) && !blackholePrefab.activeSelf && blackHoleCoolDowntime < timeCounter){
             
             blackholePrefab.SetActive(true);
             animator.SetBool("timeout", false);
