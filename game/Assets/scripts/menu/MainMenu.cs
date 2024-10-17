cd5aa2c80a9436768c84a6341addc8b9ed25fb87
Ragib Shahrier Bin Kabir <ragibshahrier2004123@gmail.com>
2024-10-17 17:05:48 +0600

    Main menu complete

--- game/Assets/scripts/menu/MainMenu.cs
+++ game/Assets/scripts/menu/MainMenu.cs
@@ -9,4 +9,8 @@ public class MainMenu : MonoBehaviour
     {
         SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex + 1);
     }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
 }
